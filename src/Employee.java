97f48af65b5582991fd57807a6539405b2e895d9
Emmanuel <emmanuel.santiago6@upr.edu>
2018-08-29 16:33:25 -0400

    We fixed the code with if else statements.

--- src/Employee.java
+++ src/Employee.java
@@ -80,9 +80,24 @@ public class Employee {
 	 */
 	public int seniorityLevel() {
 		
+		if(this.yearService>=10){
+			return 3;
+			}
+		else if (this.yearService>=5){
+			return 2;
+			
+		}
+		else if (this.yearService>=1){
+			return 1;
+			
+		}
+		else{
+			return 0;
+		}
+		
 		//Add Code Here [You have to use If/Else to get graded]
 		
-		return 0;// Temporal Return
+		// Temporal Return
 		
 	}
 }
